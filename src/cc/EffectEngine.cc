53945d96c92b2876826735628b49300ab1f705ce
Ian Wilkey <iwilkey@mail.bradley.edu>
2022-12-01 11:39:07 -0600

    Updated FaceSpace filtering.

--- src/cc/EffectEngine.cc
+++ src/cc/EffectEngine.cc
@@ -4,74 +4,90 @@
 #include "../h/FaceSpaceCore.h"
 #include "../h/EffectEngine.h"
 
-EffectEngine::EffectEngine() {
-
-    isGrayScale = false;
-    isSepia = false;
-    isSharp = false;
-    isPencil = false;
-    isDetailEnhance = false;
-    isInverted = false;
-    brightness = 1.0f;
-
-    sepiaKernel =
-            (Mat_<float>(3, 3)
-                <<
-                0.272, 0.534, 0.131,
-                0.349, 0.686, 0.168,
-                0.393, 0.769, 0.189);
-
-    sharpenKernel = 
-            (Mat_<float>(3, 3)
-                <<
-                -1, -1, -1,
-                -1, 9.5, -1,
-                -1, -1, -1);
-
-}
+EffectEngine::EffectEngine() {}
 
 EffectEngine::~EffectEngine() {}
 
 void EffectEngine::gui(void) {
     ImGui::Begin("Effect Engine");
     ImGui::Text("Use the Effect Engine to alter your FaceSpace!");
-
     ImGui::Separator();
     ImGui::Text("Filtering");
-    ImGui::Checkbox("Grayscale", &isGrayScale);
-    ImGui::Checkbox("Sepia", &isSepia);
-    ImGui::Checkbox("Sharpen", &isSharp);
-    ImGui::Checkbox("Pencil sketch", &isPencil);
-    ImGui::Checkbox("Enhance detail (HDR)", &isDetailEnhance);
-    ImGui::Checkbox("Invert", &isInverted);
+    ImGui::Checkbox("Apply grayscale (dimensional conversion)", &effectState[GRAYSCALE_FILTER_BIT]);
+    ImGui::Checkbox("Apply sepia (2D filter)", &effectState[SEPIA_FILTER_BIT]);
+    ImGui::Checkbox("Apply sharpening (2D filter)", &effectState[SHARP_FILTER_BIT]);
+    ImGui::Checkbox("Apply inverting (logical NOT)", &effectState[INVERTING_FILTER_BIT]);
+    ImGui::Checkbox("Apply box blur (2D filter)", &effectState[BOX_BLUR_BIT]);
+    ImGui::Checkbox("Apply guassian blur (2D filter)", &effectState[GAUSSIAN_BLUR_BIT]);
+    ImGui::Checkbox("Apply edge detection (2D filter)", &effectState[EDGE_DETECTION_BIT]);
+    ImGui::Checkbox("Apply custom filter (2D filter)", &effectState[CUSTOM_FILTER_BIT]);
     ImGui::SliderFloat("Brightness", &brightness, 0, 5.0f);
-
     ImGui::End();
 }
 
 void EffectEngine::process(void) {
-
     this->output = FaceSpaceCore::pollWebcam();
+    applyBrightness();
 
-    convertScaleAbs(this->output, this->output, brightness);
-
-    if(isGrayScale)
-        cvtColor(this->output, this->output, COLOR_BGR2GRAY);
-    if(isSepia && !isGrayScale)
-        transform(this->output, this->output, sepiaKernel);
-    else isSepia = false; // Sepia cannot process during grayscale because of dim change...
-    if(isSharp)
-        filter2D(this->output, this->output, -1, sharpenKernel);
-    if(isPencil && !isGrayScale) {
-        Mat bwp;
-        pencilSketch(this->output, bwp, this->output, 60, 0.01f, 0.03f); // These functions are not very performant...
-    } else isPencil = false;
-    if(isDetailEnhance && !isGrayScale) 
-        detailEnhance(this->output, this->output, 12, 0.15); // These functions are not very performant...
-    else isDetailEnhance = false;
-    if(isInverted)
-        bitwise_not(this->output, this->output);
+    // These two filters cannot exist together because of dimensional differences.
+    if(effectState[GRAYSCALE_FILTER_BIT] && effectState[SEPIA_FILTER_BIT]) {
+        effectState[GRAYSCALE_FILTER_BIT] = false;
+        effectState[SEPIA_FILTER_BIT] = false;
+    }
+    
+    if(effectState[GRAYSCALE_FILTER_BIT])
+        applyGrayscaleFilter();
+    if(effectState[SEPIA_FILTER_BIT])
+        applySepiaFilter();
+    if(effectState[SHARP_FILTER_BIT])
+        applySharpFilter();
+    if(effectState[INVERTING_FILTER_BIT])
+        applyInvertingFilter();
+    if(effectState[BOX_BLUR_BIT])
+        applyBoxBlurFilter();
+    if(effectState[GAUSSIAN_BLUR_BIT])
+        applyGaussianBlurFilter();
+    if(effectState[EDGE_DETECTION_BIT])
+        applyEdgeDetectionFilter();
+    if(effectState[CUSTOM_FILTER_BIT])
+        applyCustomFilter();
 
 }
 
-#endif // _FACE_EFFECT_CC_
+void EffectEngine::applyGrayscaleFilter(void) {
+    cvtColor(this->output, this->output, COLOR_BGR2GRAY);
+}
+
+void EffectEngine::applySepiaFilter(void) {
+    transform(this->output, this->output, SEPIA_KERNEL);
+}
+
+void EffectEngine::applySharpFilter(void) {
+    filter2D(this->output, this->output, -1, SHARPEN_KERNEL);
+}
+
+void EffectEngine::applyInvertingFilter(void) {
+    bitwise_not(this->output, this->output);
+}
+
+void EffectEngine::applyBoxBlurFilter(void) {
+    filter2D(this->output, this->output, -1, BOX_BLUR_KERNEL);
+}
+
+void EffectEngine::applyGaussianBlurFilter(void) {
+    filter2D(this->output, this->output, -1, GAUSSIAN_BLUR_KERNEL);
+}
+
+void EffectEngine::applyEdgeDetectionFilter(void) {
+    filter2D(this->output, this->output, -1, EDGE_DETECTION_KERNEL);
+}
+
+void EffectEngine::applyCustomFilter(void) {
+    filter2D(this->output, this->output, -1, customKernel);
+}
+
+void EffectEngine::applyBrightness(void) {
+    convertScaleAbs(this->output, this->output, brightness);
+}
+
+#endif // _EFFECT_ENGINE_CC_
--- src/h/EffectEngine.h
+++ src/h/EffectEngine.h
@@ -2,10 +2,23 @@
 #define _EFFECT_ENGINE_H_
 
 #include <iostream>
+#include <vector>
 #include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
+
+#define GAUSSIAN_MATRIX_CONSTANT 1 / 256.0f
+
+#define FILTER_COUNT 8
+#define GRAYSCALE_FILTER_BIT 0x00
+#define SEPIA_FILTER_BIT 0x01
+#define SHARP_FILTER_BIT 0x02
+#define INVERTING_FILTER_BIT 0x03
+#define BOX_BLUR_BIT 0x04
+#define GAUSSIAN_BLUR_BIT 0x05
+#define EDGE_DETECTION_BIT 0x06
+#define CUSTOM_FILTER_BIT 0x07
 
 class EffectEngine {
     public:
@@ -17,21 +30,64 @@ class EffectEngine {
         inline Mat * poll(void) { return &output; }
 
     private:
-        // Output image to renderer
+
+        void applyGrayscaleFilter(void);
+        void applySepiaFilter(void);
+        void applySharpFilter(void);
+        void applyInvertingFilter(void);
+        void applyBoxBlurFilter(void);
+        void applyGaussianBlurFilter(void);
+        void applyEdgeDetectionFilter(void);
+        void applyCustomFilter(void);
+        void applyBrightness(void);
+
+        // Output image to renderer...
         Mat output;
 
-        // Kernels
-        Mat sepiaKernel;
-        Mat sharpenKernel;
+        // Common kernels...
+        const Mat SEPIA_KERNEL =
+            (Mat_<float>(3, 3)
+                <<
+                0.272, 0.534, 0.131,
+                0.349, 0.686, 0.168,
+                0.393, 0.769, 0.189);
+        const Mat SHARPEN_KERNEL = 
+            (Mat_<float>(3, 3)
+                <<
+                -1, -1,  -1,
+                -1, 9.5, -1,
+                -1, -1,  -1);
+        const Mat BOX_BLUR_KERNEL = 
+            (Mat_<float>(3, 3)
+                <<
+                (1 / 9.0f), (1 / 9.0f), (1 / 9.0f),
+                (1 / 9.0f), (1 / 9.0f), (1 / 9.0f),
+                (1 / 9.0f), (1 / 9.0f), (1 / 9.0f));
+        const Mat GAUSSIAN_BLUR_KERNEL = 
+            (Mat_<float>(5, 5)
+                <<
+                (GAUSSIAN_MATRIX_CONSTANT * 1), (GAUSSIAN_MATRIX_CONSTANT * 4),  (GAUSSIAN_MATRIX_CONSTANT * 6),  (GAUSSIAN_MATRIX_CONSTANT * 4),  (GAUSSIAN_MATRIX_CONSTANT * 1),
+                (GAUSSIAN_MATRIX_CONSTANT * 4), (GAUSSIAN_MATRIX_CONSTANT * 16), (GAUSSIAN_MATRIX_CONSTANT * 24), (GAUSSIAN_MATRIX_CONSTANT * 16), (GAUSSIAN_MATRIX_CONSTANT * 4),
+                (GAUSSIAN_MATRIX_CONSTANT * 6), (GAUSSIAN_MATRIX_CONSTANT * 24), (GAUSSIAN_MATRIX_CONSTANT * 36), (GAUSSIAN_MATRIX_CONSTANT * 24), (GAUSSIAN_MATRIX_CONSTANT * 6),
+                (GAUSSIAN_MATRIX_CONSTANT * 4), (GAUSSIAN_MATRIX_CONSTANT * 16), (GAUSSIAN_MATRIX_CONSTANT * 24), (GAUSSIAN_MATRIX_CONSTANT * 16), (GAUSSIAN_MATRIX_CONSTANT * 4),
+                (GAUSSIAN_MATRIX_CONSTANT * 1), (GAUSSIAN_MATRIX_CONSTANT * 4),  (GAUSSIAN_MATRIX_CONSTANT * 6),  (GAUSSIAN_MATRIX_CONSTANT * 4),  (GAUSSIAN_MATRIX_CONSTANT * 1));
+        const Mat EDGE_DETECTION_KERNEL = 
+            (Mat_<float>(3, 3)
+                <<
+                -1, -1, -1,
+                -1,  8, -1,
+                -1, -1, -1);
+        // Init as identity kernel...
+        Mat customKernel =
+            (Mat_<float>(3, 3)
+                <<
+                0, 0, 0,
+                0, 1, 0,
+                0, 0, 0);
 
         // Effect states
-        bool isGrayScale;
-        bool isSepia;
-        bool isSharp;
-        bool isPencil;
-        bool isDetailEnhance;
-        bool isInverted;
-        float brightness;
+        bool effectState[FILTER_COUNT] = { false };
+        float brightness = 1.0f;
 };
 
-#endif // _FACE_EFFECT_H_
+#endif // _EFFECT_ENGINE_H_
